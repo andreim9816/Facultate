dd30e3d55a0f73fc7017ec5b18738a59321612a4
Andrei Manolache <48209373+andreim9816@users.noreply.github.com>
2020-02-13 01:22:54 +0200

    Add files via upload

---
+++ Anul II/Proiect/src/decriptare.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"multiple_use.h"
+
+int decrypt_cuv(char**, int, char*,unsigned int *, unsigned int);
+
+int gen_perm_invers(unsigned int **, int, unsigned int *);
+
+int decriptare(char*, char*, char *);
