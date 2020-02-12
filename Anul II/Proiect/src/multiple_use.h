e67126827e42c16c5d87ecee131d48aabf84cb32
Andrei Manolache <48209373+andreim9816@users.noreply.github.com>
2020-02-13 01:28:25 +0200

    Delete multiple_use.h

--- Anul II/Proiect/src/multiple_use.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-void XorShift32(unsigned int**, unsigned int, unsigned int );
-
-void Durstenfeld(unsigned int**, int , unsigned int*);
-
-int permut(char**, char*, unsigned int*, unsigned int );
-
-int save_shared_memory(FILE*, int, int, char*);
